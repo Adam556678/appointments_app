eb06e4862a198229ccd18641264162f29e7aaa81
Adam556678 <mahmudramadan75@yahoo.com>
2024-01-04 19:06:19 +0200

    system architecture

---
+++ lib/appointment_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppointmentApp extends StatelessWidget {
+  const AppointmentApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
